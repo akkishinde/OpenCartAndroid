71f433f99c4750af90e3d9104d7d0a2671fedd9e
akshay <akshay11.shinde@gmail.com>
2015-03-24 18:34:54 +0530

    Register Activity Phase 1

--- app/src/main/java/com/splashinfotech/mahaveer_test/LoginActivity.java
+++ app/src/main/java/com/splashinfotech/mahaveer_test/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.splashinfotech.mahaveer_test;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Akshay on 3/24/2015.
@@ -13,5 +16,11 @@ public class LoginActivity extends Activity{
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.login_activity);
+
   }
+    public void register(View view)
+    {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/splashinfotech/mahaveer_test/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.splashinfotech.mahaveer_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Splash New on 3/24/2015.
+ */
+public class RegisterActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+
+}
