55fab741de9bafad08cc84ef678f0a94d701d119
akshay <akshay11.shinde@gmail.com>
2015-03-24 18:06:45 +0530

    Login Screen Design Done

--- app/src/main/java/com/splashinfotech/mahaveer_test/LoginActivity.java
+++ app/src/main/java/com/splashinfotech/mahaveer_test/LoginActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
  * Created by Akshay on 3/24/2015.
  */
 public class LoginActivity extends Activity{
+
+
   @Override
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
